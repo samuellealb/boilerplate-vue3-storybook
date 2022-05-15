8f8de10efe94813183a930b2c2c6945ce67a079c
Samuel Leal <samuel.leal>
2022-05-15 20:28:02 +0000

    Storybook addons

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -5,7 +5,16 @@ module.exports = {
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    'storybook-dark-mode',
+    {
+      name: '@storybook/addon-storysource',
+      options: {
+        loaderOptions: {
+          injectStoryParameters: false,
+        },
+      },
+    },
   ],
   "framework": "@storybook/vue3",
   "core": {
